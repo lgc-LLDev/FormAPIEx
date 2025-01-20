d37424402edf8b3407ab5a4c9b0b880ab32af76d
student_2333 <lgc2333@126.com>
2025-01-21 02:31:56 +0800

    up

--- src/const.ts
+++ src/const.ts
@@ -4,7 +4,7 @@ export const NAME = 'FormAPIEx'
 export const VERSION = <[number, number, number]>(
   version.split('.').map((v) => Number(v))
 )
-export const AUTHOR = 'student_2333 <lgc2333@126.com>'
+export const AUTHOR = 'LgCookie <lgc2333@126.com>'
 export const LICENSE = 'Apache-2.0'
 
 export const FormClose = Symbol(`${NAME}_FormClose`)
